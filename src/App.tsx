184c9195d660cbbf08e03276d093514fbe3e541a
Rodolpho Reis <rodolphoreis@live.com>
2024-09-18 17:40:54 +0100

    feat: Add Header component and configure Router in App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Header } from "./components/Header";
+import { Router } from "./Router";
+
+
 export function App() {
   return (
     <>
-      <h1 className="bg-red-200">Hello World</h1>
+    <Header/>
+    <main className="w-full h-full">
+      
+      <div className=" bg-zinc-700 max-w-screen-xl mx-auto mt-40 h-96 px-4 py-4 justify-center items-center rounded-xl">
+     <Router/>
+      </div>
+    </main>
     </>
+    
+    
   );
 }
