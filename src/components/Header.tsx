5e815663b40253f318f63671e01c78731424d107
Rodolpho Reis <rodolphoreis@live.com>
2024-09-18 17:41:13 +0100

    feat: Add Header component with navigation icons and styling

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import { BiDownArrowAlt } from "react-icons/bi";
+import { MdTimer } from "react-icons/md";
+
+export function Header() {
+  return (
+    <header className="w-full h-24 bg-zinc-800 items-center border-b-2">
+      <div className=" flex  max-w-screen-xl h-full mx-auto my-0  justify-between ">
+        <h1 className="text-yellow-400 font-mono text-3xl font-bold items-center self-center cursor-pointer">
+          Time
+        </h1>
+        <div className="flex gap-10">
+          <a
+            href="/history"
+            className="self-center text-zinc-300 hover:text-yellow-400 cursor-pointer"
+          >
+            <MdTimer size={24} />
+          </a>
+          <a
+            href="/history"
+            className="self-center text-zinc-300 hover:text-yellow-400 cursor-pointer"
+          >
+            <BiDownArrowAlt size={32} />
+          </a>
+        </div>
+      </div>
+    </header>
+  );
+}
