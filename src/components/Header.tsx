3489172a378c6d33e8ae01c1ebb58ffc4cc65918
Rodolpho Reis <rodolphoreis@live.com>
2024-09-19 17:11:04 +0100

    style: update header styling with padding and link color adjustments

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,21 +3,21 @@ import { MdTimer } from "react-icons/md";
 
 export function Header() {
   return (
-    <header className="w-full h-24 bg-zinc-800 items-center border-b-2">
+    <header className="w-full h-24 bg-zinc-800 items-center border-b-2 p-5">
       <div className=" flex  max-w-screen-xl h-full mx-auto my-0  justify-between ">
-        <h1 className="text-yellow-400 font-mono text-3xl font-bold items-center self-center cursor-pointer">
+        <h1 className="text-yellow-400 font-mono text-3xl font-bold items-center self-center cursor-pointer ">
           Time
         </h1>
         <div className="flex gap-10">
           <a
-            href="/history"
-            className="self-center text-zinc-300 hover:text-yellow-400 cursor-pointer"
+            href="/"
+            className="self-center text-yellow-400 hover:text-zinc-400 cursor-pointer"
           >
             <MdTimer size={24} />
           </a>
           <a
             href="/history"
-            className="self-center text-zinc-300 hover:text-yellow-400 cursor-pointer"
+            className="self-center text-yellow-400 hover:text-zinc-400 cursor-pointer"
           >
             <BiDownArrowAlt size={32} />
           </a>
