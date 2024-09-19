83d6403c603a5c06d1a4d5da244a453ba44e12f9
Rodolpho Reis <rodolphoreis@live.com>
2024-09-19 17:10:21 +0100

    fix: adjust layout and spacing for main content and router components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import { Header } from "./components/Header";
 import { Router } from "./Router";
 
-
 export function App() {
   return (
     <>
-    <Header/>
-    <main className="w-full h-full">
-      
-      <div className=" bg-zinc-700 max-w-screen-xl mx-auto mt-40 h-96 px-4 py-4 justify-center items-center rounded-xl">
-     <Router/>
-      </div>
-    </main>
+      <Header />
+      <main className="w-full h-full">
+        <div className="px-2 ">
+          <div className=" max-w-screen-xl mx-auto mt-20 xl:mt40 h-96 px-4 py-4 justify-center items-center rounded-xl">
+            <Router />
+          </div>
+        </div>
+      </main>
     </>
-    
-    
   );
 }
