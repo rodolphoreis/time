b6a417afa69a4a8d9ba108533c6cf81b4b5fde3e
Rodolpho Reis <rodolphoreis@live.com>
2024-09-19 17:15:06 +0100

    refactor: replace default inputs and buttons with custom Input component and add datalist for task suggestions

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,15 +1,17 @@
 import { MdPlayArrow } from "react-icons/md";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export function Home() {
   return (
     <div className="flex-1 flex-wrap">
       <form className=" m-1 flex flex-wrap flex-col ">
-        <div className="flex justify-center mb-5 gap-2 items-center flex-wrap sm:flex-nowrap">
-          <label htmlFor="task" className="text-sm  sm:text-3xl ">
-            Vou trabalhar em
-          </label>
-          <input
-            type="text"
+        <div className="flex gap-2 ">
+          <Input
+            placeholder="Dê o nome para sua tarefa..."
+            list="task-suggestions"
+            className="bg-transparent outline-2 border-b  border-gray-500 outline-none flex flex-grow"
+          />
           <datalist id="task-suggestions" className="w-full">
             <option value="Estudar NodeJs" />
             <option value="Estudar React" />
@@ -18,40 +20,38 @@ export function Home() {
             <option value="Estudar Go" />
             <option value="Estudar Rust" />
           </datalist>
-          <input
+          <Input
             type="number"
-            id="minutesAmount"
-            className="border-b border-gray-500 w-24 text-sm sm:text-3xl bg-transparent outline-2 focus:border-b-2 focus:border-blue-500 placeholder:text-zinc-500 placeholder:pl-2"
+            step={5}
+            min={5}
+            max={60}
+            placeholder="Durante..."
+            className="bg-transparent outline-2 border-b border-gray-500 outline-none w-32"
           />
-
-          <span className="text-sm sm:text-3xl">minutos.</span>
         </div>
 
         <div className="flex justify-center mb-2 gap-2 items-center mt-16">
-          <span className="bg-zinc-800 p-3 text-6xl sm:p-6 sm:text-8xl  rounded-xl font-mono">
+          <span className="bg-zinc-600 p-3 text-6xl sm:p-6 sm:text-8xl  rounded-xl font-mono">
             0
           </span>
-          <span className="bg-zinc-800 p-3 text-6xl sm:p-6 sm:text-8xl  rounded-xl font-mono">
+          <span className="bg-zinc-600 p-3 text-6xl sm:p-6 sm:text-8xl  rounded-xl font-mono">
             0
           </span>
           <span className="text-8xl text-yellow-400 font-extrabold">:</span>
-          <span className="bg-zinc-800 p-3 text-6xl sm:p-6 sm:text-8xl  rounded-xl font-mono">
+          <span className="bg-zinc-600 p-3 text-6xl sm:p-6 sm:text-8xl  rounded-xl font-mono">
             0
           </span>
-          <span className="bg-zinc-800 p-3 text-6xl sm:p-6 sm:text-8xl  rounded-xl font-mono">
+          <span className="bg-zinc-600 p-3 text-6xl sm:p-6 sm:text-8xl  rounded-xl font-mono">
             0
           </span>
         </div>
-
-        <button
-          type="submit"
-          className="flex gap-2 text-zinc-800 justify-center bg-yellow-400  mx-2 xl:mx-32 mt-16 p-2 rounded-md cursor-pointer hover:bg-yellow-300 "
+        <Button
+          variant="outline"
+          className=" bg-yellow-400 text-xl text-black font-extrabold cursor-pointer outline-none border-none  mt-24 hover:bg-yellow-300 font-mono tracking-wide"
         >
-          <span>
-            <MdPlayArrow size={30} />
-          </span>
-          <span className=" text-2xl">Começar</span>
-        </button>
+          {" "}
+          <MdPlayArrow size={30} /> Começar
+        </Button>
       </form>
     </div>
   );
