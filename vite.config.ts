33374d06b881ce4b6d70691c73926321b1e00be9
Rodolpho Reis <rodolphoreis@live.com>
2024-09-19 17:09:51 +0100

    build: add path alias for '@' and import path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
