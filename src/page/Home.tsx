9122e10e41837e860613b8879fa136d168e2d8dc
Rodolpho Reis <rodolphoreis@live.com>
2024-09-25 17:28:48 +0100

    feat: wrap application with CyclesContext.Provider to pass cycles and activeCycleId as context values

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -113,77 +113,79 @@ export function Home() {
   }
 
   return (
-    <div className="flex-1 flex-wrap">
-      <form
-        className=" m-1 flex flex-wrap flex-col "
-        onSubmit={handleSubmit(handleCreateNewCicle)}
-      >
-        <div className="flex gap-2 ">
-          <Input
-            {...register("task")}
-            placeholder="Dê o nome para sua tarefa..."
-            list="task-suggestions"
-            className="bg-transparent outline-2 border-b  border-gray-500 outline-none flex flex-grow"
-          />
-          <datalist id="task-suggestions" className="w-full">
-            <option value="Estudar NodeJs" />
-            <option value="Estudar React" />
-            <option value="Estudar Flutter" />
-            <option value="Estudar Python" />
-            <option value="Estudar Go" />
-            <option value="Estudar Rust" />
-          </datalist>
-          <Input
-            type="number"
-            {...register("duration", { required: true, valueAsNumber: true })}
-            step={5}
-            min={1}
-            max={60}
-            placeholder="00"
-            className="bg-transparent outline-2 border-b border-gray-500 outline-none w-32"
-          />
-          {errors.duration && <span>Este campo é obrigatório</span>}
-        </div>
+    <CyclesContext.Provider value={{ cycles, activeCycleId }}>
+      <div className="flex-1 flex-wrap">
+        <form
+          className=" m-1 flex flex-wrap flex-col "
+          onSubmit={handleSubmit(handleCreateNewCicle)}
+        >
+          <div className="flex gap-2 ">
+            <Input
+              {...register("task")}
+              placeholder="Dê o nome para sua tarefa..."
+              list="task-suggestions"
+              className="bg-transparent outline-2 border-b  border-gray-500 outline-none flex flex-grow"
+            />
+            <datalist id="task-suggestions" className="w-full">
+              <option value="Estudar NodeJs" />
+              <option value="Estudar React" />
+              <option value="Estudar Flutter" />
+              <option value="Estudar Python" />
+              <option value="Estudar Go" />
+              <option value="Estudar Rust" />
+            </datalist>
+            <Input
+              type="number"
+              {...register("duration", { required: true, valueAsNumber: true })}
+              step={5}
+              min={1}
+              max={60}
+              placeholder="00"
+              className="bg-transparent outline-2 border-b border-gray-500 outline-none w-32"
+            />
+            {errors.duration && <span>Este campo é obrigatório</span>}
+          </div>
 
-        <div className="flex justify-center mb-2 gap-2 items-center mt-16">
-          <span className="bg-zinc-600 p-3 text-6xl sm:p-6 sm:text-8xl  rounded-xl font-mono">
-            {minutes[0]}
-          </span>
-          <span className="bg-zinc-600 p-3 text-6xl sm:p-6 sm:text-8xl  rounded-xl font-mono">
-            {minutes[1]}
-          </span>
-          <span className="text-8xl text-yellow-400 font-extrabold">:</span>
-          <span className="bg-zinc-600 p-3 text-6xl sm:p-6 sm:text-8xl  rounded-xl font-mono">
-            {seconds[0]}
-          </span>
-          <span className="bg-zinc-600 p-3 text-6xl sm:p-6 sm:text-8xl  rounded-xl font-mono">
-            {seconds[1]}
-          </span>
-        </div>
-        {activeCycle ? (
-          <Button
-            variant="outline"
-            onClick={handleInterruptCycle}
-            type="button"
-            className=" bg-red-700 text-xl text-white font-extrabold cursor-pointer outline-none border-none  mt-24 hover:bg-red-500 hover:text-white font-mono tracking-wide"
-          >
-            {" "}
-            <MdPause size={30} />
-            Interromper
-          </Button>
-        ) : (
-          <Button
-            variant="outline"
-            disabled={isSubmitDisabled}
-            type="submit"
-            className=" bg-yellow-400 text-xl text-black font-extrabold cursor-pointer outline-none border-none  mt-24 hover:bg-yellow-300 font-mono tracking-wide"
-          >
-            {" "}
-            <MdPlayArrow size={30} />
-            Começar
-          </Button>
-        )}
-      </form>
-    </div>
+          <div className="flex justify-center mb-2 gap-2 items-center mt-16">
+            <span className="bg-zinc-600 p-3 text-6xl sm:p-6 sm:text-8xl  rounded-xl font-mono">
+              {minutes[0]}
+            </span>
+            <span className="bg-zinc-600 p-3 text-6xl sm:p-6 sm:text-8xl  rounded-xl font-mono">
+              {minutes[1]}
+            </span>
+            <span className="text-8xl text-yellow-400 font-extrabold">:</span>
+            <span className="bg-zinc-600 p-3 text-6xl sm:p-6 sm:text-8xl  rounded-xl font-mono">
+              {seconds[0]}
+            </span>
+            <span className="bg-zinc-600 p-3 text-6xl sm:p-6 sm:text-8xl  rounded-xl font-mono">
+              {seconds[1]}
+            </span>
+          </div>
+          {activeCycle ? (
+            <Button
+              variant="outline"
+              onClick={handleInterruptCycle}
+              type="button"
+              className=" bg-red-700 text-xl text-white font-extrabold cursor-pointer outline-none border-none  mt-24 hover:bg-red-500 hover:text-white font-mono tracking-wide"
+            >
+              {" "}
+              <MdPause size={30} />
+              Interromper
+            </Button>
+          ) : (
+            <Button
+              variant="outline"
+              disabled={isSubmitDisabled}
+              type="submit"
+              className=" bg-yellow-400 text-xl text-black font-extrabold cursor-pointer outline-none border-none  mt-24 hover:bg-yellow-300 font-mono tracking-wide"
+            >
+              {" "}
+              <MdPlayArrow size={30} />
+              Começar
+            </Button>
+          )}
+        </form>
+      </div>
+    </CyclesContext.Provider>
   );
 }
