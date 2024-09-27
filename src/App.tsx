1f2516323fa9ef2393005193f039641f328cf111
Rodolpho Reis <rodolphoreis@live.com>
2024-09-27 19:20:48 +0100

    feat(context): add CycleContextProvider to wrap Router for global state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header";
+import { CycleContextProvider } from "./page/Home";
 import { Router } from "./Router";
 
 export function App() {
@@ -8,7 +9,9 @@ export function App() {
       <main className="w-full h-full">
         <div className="px-2 ">
           <div className=" max-w-screen-xl mx-auto mt-20 xl:mt40 h-96 px-4 py-4 justify-center items-center rounded-xl">
-            <Router />
+            <CycleContextProvider>
+              <Router />
+            </CycleContextProvider>
           </div>
         </div>
       </main>
