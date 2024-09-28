657d3dc8330ac484dc828e54b83ae8fa3f6ceb0d
Rodolpho Reis <rodolphoreis@live.com>
2024-09-28 13:39:53 +0100

    refactor: remove CycleContextProvider from Router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Header } from "./components/Header";
-import { CycleContextProvider } from "./context/CycleContextProvider";
 
 import { Router } from "./Router";
 
@@ -10,9 +9,7 @@ export function App() {
       <main className="w-full h-full">
         <div className="px-2 ">
           <div className=" max-w-screen-xl mx-auto mt-20 xl:mt40 h-96 px-4 py-4 justify-center items-center rounded-xl">
-            <CycleContextProvider>
-              <Router />
-            </CycleContextProvider>
+            <Router />
           </div>
         </div>
       </main>
