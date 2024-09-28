ca0881cbcceec75ba8a4e39fe46268860b5c0147
Rodolpho Reis <rodolphoreis@live.com>
2024-09-28 13:39:20 +0100

    feat: reorganize router to include CycleContextProvider for state management

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,17 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-
 import { Home } from "./page/Home";
 import { History } from "./page/History";
+import { CycleContextProvider } from "./context/CycleContextProvider";
 
 export function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/history" element={<History />} />
-      </Routes>
+      <CycleContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />
+        </Routes>
+      </CycleContextProvider>
     </BrowserRouter>
   );
 }
